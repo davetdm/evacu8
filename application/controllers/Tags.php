a59352b040a9cf7fb044321c03f1a6a67d80b575
davetdm <thatomashifane@gmail.com>
2019-10-25 09:50:57 +0200

    Edited Files

--- application/controllers/Tags.php
+++ application/controllers/Tags.php
@@ -17,33 +17,7 @@ class Tags extends CI_Controller{
     }
 
     public function add(){
-        if ($this->input->method() != "post"){
-            echo utils::response("Invalid request method", "error");
-            return false;
-        }
-        try {
-            echo "<pre/>";
-            $this->db->trans_begin();
-            $data = array(
-                'id'=>strip_tags($this->input->post('id')),
-                'date_added' =>date('Y-m-d',strtotime(utils::getDate()))              
-                );
-            $result = $this->TagsModel->add_tag($data);
-            
-            if ($this->db->trans_status() === FALSE) {
-                $this->db->trans_rollback();
-                echo utils::response("Operation failed!", "error");
-                return false;
-            } else {
-                $this->db->trans_commit();
-                echo utils::response("Operation successful", "ok");
-                return true;
-            }
-        } catch (Exception $e) {
-            print_r($e->getMessage());
-            echo utils::response("Could not register this time. Please try again later!", "error");
-            return false;
-        }
+        
     }
 
     public function assign(){
